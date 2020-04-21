9eab9ea07493fb54fe166a1eb34a87d47a6f3cff
Charlie Mordant <cmordant1@gmail.com>
2020-04-21 15:07:50 +0200

    configure static NAT ip for VM

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,3 +5,9 @@ def test_vm_has_started(host):
     command = r"""sudo virsh list | egrep -c 'fedoraGuestVM\s*running'"""
     cmd = host.run(command)
     assert '1' in cmd.stdout
+
+
+def test_vm_ip_has_been_defined(host):
+    command = r"""sudo virsh net-dumpxml default  | xmllint --xpath '/network/ip/dhcp/host' - | grep -c 'ip='"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
