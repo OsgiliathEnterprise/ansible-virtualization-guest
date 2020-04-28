50ccfc4f026d095682390db0c6e13b4ef8305f4a
Charlie Mordant <cmordant1@gmail.com>
2020-04-29 00:21:54 +0200

    secured vm login via ssh

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -15,7 +15,7 @@ def test_vm_ip_has_been_defined(host):
     assert '1' in cmd.stdout
 
 
-def test_port_is_opened(host):
+def test_host_port_is_opened(host):
     a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     location = ("127.0.0.1", 6752)
@@ -25,3 +25,31 @@ def test_port_is_opened(host):
         print("Port is open")
     else:
         assert 0 == 1
+
+
+def test_vagrant_ssh_private_key_is_downloaded(host):
+    command = """stat ~/.ssh/id_rsafedoraGuestVM  | \
+    grep -c 'File'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_vagrant_ssh_public_key_is_downloaded(host):
+    command = """stat ~/.ssh/id_rsafedoraGuestVM.pub  | \
+    grep -c 'File'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_shell_login_to_vm_is_disabled(host):
+    retreiveipcommand = r""" sudo virsh net-dumpxml default | \
+    xmllint --xpath '/network/ip/dhcp/host/@ip' - | \
+    sed 's/^[^"]*"\([^"]*\)".*/\1/'"""
+    ip = host.run(retreiveipcommand)
+    sshcommand = """
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
+    vagrant@%s 2>&1""" % ip.stdout
+    cmd = host.run(sshcommand)
+    print('------')
+    print(cmd.stderr)
+    assert 'Permission denied' in cmd.stderr
